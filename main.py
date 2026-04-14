87c27ab56b4ddef0c2217b2479344b02c4eab257
deepradhan <dpkpradhan649@gmail.com>
2026-04-15 00:23:35 +0530

    updated the code to fetch birth details from header

--- main.py
+++ main.py
@@ -3,19 +3,11 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-# Request body
-class BirthDetails(BaseModel):
-    date: str
-    time: str
-    lat: float
-    lon: float
-
 @app.get("/")
 def home():
     return {"message": "Astrology API is running"}
 
 
 @app.post("/kundli")
-def generate_kundli(data: BirthDetails):
-    result = "Hi"
-    return {"kundli": result}
+def generate_kundli(name: str,dob: str,lat: float, lon: float):
+    return {"kundli": name + dob + lat +lon}
