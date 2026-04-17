bdbe3e48417e9a2ab4d241fe4f15bc09641ce9d1
deepradhan <dpkpradhan649@gmail.com>
2026-04-18 03:09:15 +0530

    updated structure

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+from routes.kundli import router as kundli_router
 app = FastAPI()
-
+app.include_router(kundli_router)
 @app.get("/")
 def home():
     return {"message": "Astrology API is running"}
--- routes/kundli.py
+++ routes/kundli.py
@@ -1,10 +1,7 @@
 from fastapi import APIRouter
-
 from services.kundli_service import generate_kundli
-
 router = APIRouter()
 
-
 @router.post("/kundli")
-def generate_route(name: str, dob: str, lat: float, lon: float):
+def kundli_route(name: str, dob: str, lat: float, lon: float):
     return generate_kundli(name, dob, lat, lon)
