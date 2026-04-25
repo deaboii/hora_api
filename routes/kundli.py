db74c5b0746972cba399f2e61443378abc0949ff
deepradhan <dpkpradhan649@gmail.com>
2026-04-26 04:12:28 +0530

    added yogas3

--- routes/kundli.py
+++ routes/kundli.py
@@ -6,4 +6,3 @@ router = APIRouter()
 def kundli_route(name: str, dob: str,birth_time:str, lat: float, lon: float):
     return generate_kundli(name, dob,birth_time, lat, lon)
 
-print(kundli_route("deepak","09-01-1997","23.49",21.39,85.38))
