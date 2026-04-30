982c64eea0f836ce7035e3d8b9532fbba82f42ca
deepradhan <dpkpradhan649@gmail.com>
2026-05-01 04:36:02 +0530

    added hi

--- main.py
+++ main.py
@@ -10,9 +10,11 @@ app.include_router(kundli_router)
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_API = f"https://api.telegram.org/bot{BOT_TOKEN}"
 
+
 @app.get("/")
 def home():
     return {"message": "Astrology API is running"}
+
 
 @app.post("/webhook/astro123")
 async def telegram_webhook(req: Request):
@@ -26,12 +28,13 @@ async def telegram_webhook(req: Request):
             reply = "Welcome! Enter your birth date (YYYY-MM-DD)"
         else:
             try:
-                res = requests.post(
-                    "https://hora-api-v46s.onrender.com/predict",
-                    headers={"x-api-key": "YOUR_SECRET_KEY"},
-                    json={"input": text}
-                )
-                reply = res.json().get("prediction", "Couldn't generate prediction")
+                # res = requests.post(
+                #     "https://hora-api-v46s.onrender.com/predict",
+                #     headers={"x-api-key": "YOUR_SECRET_KEY"},
+                #     json={"input": text}
+                # )
+                # reply = res.json().get("prediction", "Couldn't generate prediction")
+                reply = "hi"
             except:
                 reply = "Error connecting to astrology service"
 
